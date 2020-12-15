e19c3d26c538b05b79f275514853a9ee3cb24b46
MARJAOUI <marjaoui@orange.fr>
2020-12-15 18:55:47 +0100

    premier envoi

--- controllers/user.js
+++ controllers/user.js
@@ -4,13 +4,17 @@ const jwt = require('jsonwebtoken'); // importation de JWT pour la création et 
 const passwordValidator = require('password-validator'); //importation de password-validator
 const User = require('../models/user');
 const mongoMask = require('mongo-mask');
-
+/*var blacklist = ['email'];
+const maskjson = require('mask-json')(blacklist);*/
+//const maskjson = require('mask-json');
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)   // algorythme de cryptage du mot de passe: fonction asynchrone
     .then(hash => {                   // mot de pass sous forme d'algotythme
       const user = new User({        //  déclaration d'un nouvel utilisateur avec pour objet
-      email: mongoMask.maskEmail(req.body.email),
+      //email: (req.body.email),      //  adresse mail
+       // email: maskData.maskEmail2(req.body.email),
+        email: mongoMask(req.body.email),
         password: hash             // mot de passe créé et crypté
       });
       //console.log(email);
@@ -20,6 +24,25 @@ exports.signup = (req, res, next) => {
     })
     .catch(error => res.status(500).json({ error: 'Le mot de passe ne respecte pas les prérequis préconisés'}));
 };
+
+
+
+
+
+
+/*exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)   // algorythme de cryptage du mot de passe: fonction asynchrone
+    .then(hash => {                   // mot de pass sous forme d'algotythme
+      const user = new User({        //  déclaration d'un nouvel utilisateur avec pour objet
+      email: (req.body.email),
+      password: hash             // mot de passe créé et crypté
+      })
+      user.save()
+        .then(() =>res.status(201).json({ message: 'Utilisateur créé !' })
+        .catch(error => res.status(400).json({ error }));
+    })
+       .catch(error => res.status(500).json({ error: 'Le mot de passe ne respecte pas les prérequis préconisés'}));
+};*/
 
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email }) // récupération du user avec l'adresse mail
